45c0bed0449ed71f587938a0bb73b7265554f1d7
shoxrux0807 <shoxruxxon.tuitkf@gmail.com>
2021-07-13 08:45:16 +0500

    first commit

---
+++ namuna.js
@@ -0,0 +1,50 @@
+"use strict";
+
+//console.log(1);
+//alert("salom");
+
+//const yosh = confirm("Siz 18 yoshdamisiz?");
+//console.log("yosh");
+
+// const ism = prompt("Ismiz nima?", "Ismizni kiriting!");
+// console.log(ism);
+
+// const Savollar = [];
+// Savollar[0] = prompt("Ismiz nima", "Valijon");
+// Savollar[1] = prompt("Familayngiz");
+// Savollar[2] = prompt("Yoshiz nechada");
+// console.log(Savollar);
+
+// const Ism = prompt("Ismizni kiriting?");
+// console.log(Ism + 97);
+
+// const ism = "shoxrux";
+// const yosh = "24";
+// console.log(`Meni ismim ${ism} va yoshim ${yosh}`);
+
+// const xName = prompt("Ismizni kiriting!");
+// console.log(`Uer's name ${xName}`);
+
+// const bir = "b";
+// console.log(typeof bir);
+
+// const x = 10,
+//   y = 15;
+
+// console.log("Yig'indi ", +x + y);
+
+// let plus = 99;
+// let minus = 99;
+
+// console.log(plus++);
+// console.log(minus--);
+
+// console.log(plus);
+// console.log(minus);
+
+// console.log(2 * 6 == "13");
+// console.log(2 * 6 === 12);
+
+const x = true,
+  y = false;
+console.log(x && !y);
